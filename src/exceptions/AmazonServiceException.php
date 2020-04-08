d7d54624e66e7566f32354a32009447a0179e452
Leo Hubert <leo.hubert@epitech.eu>
2020-04-08 11:59:56 +0200

    Add cache for userAgent up to 60 seconds. (Amazon detect random userAgent) Add $userAgent to Exception to log failed request

--- src/exceptions/AmazonServiceException.php
+++ src/exceptions/AmazonServiceException.php
@@ -4,8 +4,38 @@
 namespace AmazonService\Exceptions;
 
 use stringEncode\Exception;
+use Throwable;
 
 class AmazonServiceException extends Exception
 {
+    /**
+     * @var string $userAgent
+     */
+    private $userAgent;
+
+    public function __construct($message = "", $userAgent = null, $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->userAgent = $userAgent;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserAgent()
+    {
+        return $this->userAgent;
+    }
+
+    /**
+     * @param string $userAgent
+     * @return AmazonServiceException
+     */
+    public function setUserAgent(string $userAgent): AmazonServiceException
+    {
+        $this->userAgent = $userAgent;
+        return $this;
+    }
+
 
 }
--- src/exceptions/CouldNotProcessProduct.php
+++ src/exceptions/CouldNotProcessProduct.php
@@ -9,45 +9,49 @@ class CouldNotProcessProduct extends AmazonServiceException
     /**
      * @param string $want
      * @param array|string $divName
+     * @param string $userAgent
      * @return static
      */
-    public static function parentDivNotFound(string $want, $divName)
+    public static function parentDivNotFound(string $want, $divName, string $userAgent = null)
     {
         if (is_array($divName)) {
             $divName = implode(',', $divName);
         }
-        return new static("Could not find '$want' parentDiv with '$divName' selectors in html structure.");
+        return new static("Could not find '$want' parentDiv with '$divName' selectors in html structure.", $userAgent);
     }
 
     /**
      * @param string $want
      * @param $divName
      * @param $childDiv
+     * @param string|null $userAgent
      * @return static
      */
-    public static function childDivNotFound(string $want, $divName, $childDiv)
+    public static function childDivNotFound(string $want, $divName, $childDiv, string $userAgent = null)
     {
         if (is_array($divName)) {
             $divName = implode(',', $divName);
         }
-        return new static("Could not find '$want' childDiv with parentDiv = '$divName' | childDiv = '$childDiv' selectors in html structure.");
+        return new static("Could not find '$want' childDiv with parentDiv = '$divName' | childDiv = '$childDiv' selectors in html structure.", $userAgent);
     }
 
     /**
+     * @param string|null $userAgent
      * @return static
      */
-    public static function noCategoriesFound()
+    public static function noCategoriesFound(string $userAgent = null)
     {
-        return new static("No categories found.");
+        return new static("No categories found.", $userAgent);
     }
 
     /**
      * @param string $url
+     * @param string|null $userAgent
      * @return static
      */
-    public static function cannotRetrieveCategoryNameWithUrl(string $url)
+    public static function cannotRetrieveCategoryNameWithUrl(string $url, string $userAgent = null)
     {
-        return new static("Could not find category name from this url '$url'.");
+        return new static("Could not find category name from this url '$url'.", $userAgent);
     }
 
 
